0e24e174bc9b1d07f05c407a751c7f3b04db3ba5
정현우 <hwoo4567@gmail.com>
2022-10-03 14:11:24 +0900

    광물 녹는 속도 빠르게

--- src/main/java/com/worldedit1234/hunt/entity/OwnedFireball.java
+++ src/main/java/com/worldedit1234/hunt/entity/OwnedFireball.java
@@ -7,33 +7,39 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.LargeFireball;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 public class OwnedFireball extends LargeFireball {
     private static final Logger LOGGER = LogUtils.getLogger();
     private final Vec3 origin;
+    private final LivingEntity owner;
 
-    public OwnedFireball(Level level, LivingEntity owner, Vec3 vector) {
+    public OwnedFireball(Level level, @NotNull LivingEntity owner, Vec3 vector) {
         super(level, owner,
                 vector.x * Setup.FIREBALL_SPEED,
                 vector.y * Setup.FIREBALL_SPEED,
                 vector.z * Setup.FIREBALL_SPEED,
                 Setup.FIREBALL_POWER);
 
-        assert this.getOwner() != null;
-
-        origin = new Vec3(owner.getX(), owner.getY(), owner.getZ());
+        this.origin = new Vec3(owner.getX(), owner.getY(), owner.getZ());
+        this.owner = owner;
         LOGGER.info("New Fireball {}", this.getUUID());
     }
 
     @Override
     public void tick() {
         super.tick();
-
-        var owner = this.getOwner();
-        if (owner != null && Mth.sqrt((float) this.distanceToSqr(origin)) > Setup.MAX_FIREBALL_TRAVEL) {
+        if (Mth.sqrt((float) this.distanceToSqr(origin)) > Setup.MAX_FIREBALL_TRAVEL) {
             this.kill();
             LOGGER.info("Fireball {} is too far from origin.", this.getUUID());
         }
+    }
+
+    @NotNull
+    @Override
+    public LivingEntity getOwner() {
+        return this.owner;
     }
 }
